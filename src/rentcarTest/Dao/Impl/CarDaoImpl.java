dfd5a993567b19bb86fe7309b651a8101b7e82c2
kimchangdong <kim77246072@gmail.com>
2020-08-21 09:52:23 +0900

    Impl생성

---
+++ src/rentcarTest/Dao/Impl/CarDaoImpl.java
@@ -0,0 +1,40 @@
+package rentcarTest.Dao.Impl;
+
+import java.util.List;
+
+import rentcarTest.Dao.CarDao;
+import rentcarTest.dto.Car;
+
+public class CarDaoImpl implements CarDao{
+
+	@Override
+	public List<Car> selectCarByAll() {
+		return null;
+	}
+
+	@Override
+	public List<Car> selectCarByFind(Car car) {
+		return null;
+	}
+
+	@Override
+	public List<Car> selectCarByRent(Car car) {
+		return null;
+	}
+
+	@Override
+	public int insertCar(Car car) {
+		return 0;
+	}
+
+	@Override
+	public int updateCar(Car car) {
+		return 0;
+	}
+
+	@Override
+	public int deleteCar(Car car) {
+		return 0;
+	}
+
+}
---
+++ src/rentcarTest/Dao/Impl/MileageImpl.java
@@ -0,0 +1,45 @@
+package rentcarTest.Dao.Impl;
+
+import java.util.List;
+
+import rentcarTest.Dao.CustomerDao;
+import rentcarTest.dto.Customer;
+
+public class MileageImpl implements CustomerDao{
+
+	@Override
+	public List<Customer> selectCustomerByAll() {
+		return null;
+	}
+
+	@Override
+	public List<Customer> selectCustomerByFind(Customer ctm) {
+		return null;
+	}
+
+	@Override
+	public List<Customer> selectCustomerByRent(Customer ctm) {
+		return null;
+	}
+
+	@Override
+	public List<Customer> selectCustomerBlackList(Customer ctm) {
+		return null;
+	}
+
+	@Override
+	public int insertCustomer(Customer ctm) {
+		return 0;
+	}
+
+	@Override
+	public int updateCustomer(Customer ctm) {
+		return 0;
+	}
+
+	@Override
+	public int deleteCustomer(Customer ctm) {
+		return 0;
+	}
+
+}
