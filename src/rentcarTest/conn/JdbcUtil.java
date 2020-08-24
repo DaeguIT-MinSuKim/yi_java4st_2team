67ae6f73d0c859f82c4b7afa5d05abb8e7afbb57
bae <sbae4@students.kennesaw.edu>
2020-08-24 12:06:09 +0900

    홈 화면 패널, 기능 주석으로 정리

--- src/rentcarTest/conn/JdbcUtil.java
+++ src/rentcarTest/conn/JdbcUtil.java
@@ -10,17 +10,17 @@ import java.util.Properties;
 public class JdbcUtil {
 	public static Connection getConnection() {
 		Connection con = null;
-		String proptiesPath = "db.properties";
-		try (InputStream is = ClassLoader.getSystemResourceAsStream(proptiesPath)){
+		String propertiesPath = "db.properties";
+		try (InputStream is = ClassLoader.getSystemResourceAsStream(propertiesPath)) {
 			Properties props = new Properties();
 			props.load(is);
-			
-				String url = props.getProperty("url");
-				String user = props.getProperty("user");
-				String password = props.getProperty("password");
-				
+
+			String url = props.getProperty("url");
+			String user = props.getProperty("user");
+			String password = props.getProperty("password");
+
 			con = DriverManager.getConnection(url, props);
-				
+
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
--- test/rentcarTest/conn/JdbcUtilTest.java
+++ test/rentcarTest/conn/JdbcUtilTest.java
@@ -2,14 +2,12 @@ package rentcarTest.conn;
 
 import java.sql.Connection;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import junit.framework.Assert;
-
 
 class JdbcUtilTest {
 
