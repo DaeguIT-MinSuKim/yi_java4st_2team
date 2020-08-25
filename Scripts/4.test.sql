f15ed6a397ed361f8158535e1c43f21f66772886
kimchangdong <kim77246072@gmail.com>
2020-08-25 15:17:57 +0900

    db - test

--- Scripts/4.test.sql
+++ Scripts/4.test.sql
@@ -1,3 +1,4 @@
+-----------------------------------------------------------------------------김창동-----------------------------------------------------------------------------
 SELECT * FROM USER_TABLES ;
 
 SELECT * FROM RENT;
@@ -20,4 +21,10 @@ SELECT *
 SELECT DISTINCT c.CTM_NO, c.CTM_NAME, c.TEL, c.ADDRESS, (SELECT SUM(MILEAGE) FROM MILEAGE WHERE CTM_NO = 1) AS MILEAGE, c.CTM_REMARK 
 FROM CUSTOMER c JOIN MILEAGE m ON c.CTM_NO = m.CTM_NO WHERE c.CTM_NO = 1;
 
-SELECT CTM_NO, CTM_NAME, TEL, ADDRESS, CTM_REMARK, CTM_MLG FROM CUSTOMER WHERE CTM_NO IN(SELECT R.CTM_NO FROM RENT r LEFT OUTER JOIN CUSTOMER c ON r.CTM_NO = c.CTM_NO WHERE IS_RENT=1) ORDER BY CTM_NO; 
+SELECT CTM_NO, CTM_NAME, TEL, ADDRESS, CTM_REMARK, CTM_MLG FROM CUSTOMER WHERE CTM_NO IN(SELECT R.CTM_NO FROM RENT r LEFT OUTER JOIN CUSTOMER c ON r.CTM_NO = c.CTM_NO WHERE IS_RENT=1) ORDER BY CTM_NO;
+
+-----------------------------------------------------------------------------오수정-----------------------------------------------------------------------------
+
+-----------------------------------------------------------------------------김보현-----------------------------------------------------------------------------
+
+-----------------------------------------------------------------------------배성덕-----------------------------------------------------------------------------
