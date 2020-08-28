51cd557eec1bd0d45e32bbdbae60adc140bcc3cc
baechugongdue <candiesheis@gmail.com>
2020-08-28 09:23:19 +0900

    MileagePanel

---
+++ src/rentcarTest/Dao/service/MileageService.java
@@ -0,0 +1,20 @@
+package rentcarTest.Dao.service;
+
+import java.util.List;
+
+import rentcarTest.Dao.MileageDao;
+import rentcarTest.Dao.Impl.MileageDaoImpl;
+import rentcarTest.dto.Mileage;
+
+public class MileageService {
+	private MileageDao dao = MileageDaoImpl.getInstance();
+	
+	public void insertMile(Mileage mlg) {
+		System.out.println("Inserting Mileage..");
+		dao.insertMileage(mlg);
+	}
+	
+	public List<Mileage> showMileage(){
+		return dao.selectMileageByAll();
+	}
+}
