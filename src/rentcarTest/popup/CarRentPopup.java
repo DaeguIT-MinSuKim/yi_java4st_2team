606e33ecf1b1fa5c80c83958171238fbd9c1cd96
brave <brave@DESKTOP-V2EOLAJ>
2020-08-31 05:51:32 +0900

    carRent 저장후 재 커밋

--- src/rentcarTest/popup/CarRentPopup.java
+++ src/rentcarTest/popup/CarRentPopup.java
@@ -2,6 +2,7 @@ package rentcarTest.popup;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.util.Calendar;
 import java.util.Date;
@@ -283,6 +284,10 @@ public class CarRentPopup extends JDialog implements ActionListener, ItemListene
 		tfCtmName.setText(item.getName());
 		tfTel.setText(item.getTel());
 		lblMileage3.setText(String.valueOf(item.getCtm_mlg()));
+	}
+	
+	private void checkIsDriver(ItemEvent e) {
+		
 	}
 	
 	public void actionPerformed(ActionEvent e) {
